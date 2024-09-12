a62c8925470358f2aec40ed77b641d101fa8a17e
Praveen Chandra <Praveenchandra784@gmail.com>
2024-09-12 16:23:19 +0530

    GamePortHub

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Adjust paths based on your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
